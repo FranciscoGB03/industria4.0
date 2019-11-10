87b39838b97f598e96ae264f0cb64f90df762610
FranciscoGB03 <fgonalezb@toluca.tecnm.mx>
2019-11-10 17:22:15 -0600

    avance

--- control/acceso.php
+++ control/acceso.php
@@ -6,9 +6,9 @@ $query = new Conexion();
 //Creacion de variables
 $usuario = $_POST['user'];
 $contra = $_POST['pass'];
-$consulta = $query->ConsultaUsuario('usuario', 'Nombre="' . $usuario . '" and Contrasenia="' . $contra . '"');
-$user = $consulta['Nombre'];
-$pass = $consulta['Contrasenia'];
+$consulta = $query->ConsultaUsuario('usuario', 'nombre="' . $usuario . '" and contrasenia="' . $contra . '"');
+$user = $consulta['nombre'];
+$pass = $consulta['contrasenia'];
 if (empty($user) || empty($pass)) {
     echo '<script>alert("los datos no son correctos");</script>';
     echo '<script>window.location="../index.php";</script>';
