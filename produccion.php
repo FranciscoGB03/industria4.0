bc3c990760fe8db92347b4db1f5306d895d3406a
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-11-10 02:08:09 -0600

    Primer intento de conexión a BD

--- produccion.php
+++ produccion.php
@@ -7,10 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Pagina de inicio</title>
+        <title>Produccion</title>
         <?php
             include('componentes/enlaces.php');
         ?>
+        <script type="text/javascript" src="resources/js/consulta_lote.js"></script>
     </head>
     <body>
         <div>
@@ -22,11 +23,11 @@ and open the template in the editor.
             <img src="componentes/produccion.jpg" width="300" height="500">
         </div>
         <div>
-            <div>
-                <?php
-                    include('vistas/estadoProduccion.php');
-                ?>
-            </div>
-        </div>
+            <section>
+                <input type="text" name="buscarLote" id="buscarLote" placeholder="Buscar lote">
+            </section>
+            <section id="tabla_lote">
+                
+            </section>
     </body>
 </html>
