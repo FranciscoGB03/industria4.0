3d8404a5aaf97b803b388b8f0219efe0b219ef70
sauce <sauce@LAPTOP-IC3LGFG0>
2019-11-13 15:44:44 -0600

    modificación de navbar,y creación de temperatura.php por: FER

--- vistas/navbar.php
+++ vistas/navbar.php
@@ -5,16 +5,22 @@
     </button>
     <div class = "collapse navbar-collapse" id = "navbarSupportedContent">
         <ul class = "navbar-nav nav-centrado">
+             <li class = "nav-item active">
+                <a class = "nav-link white-text" href = "inicio.php">Inicio</a>
+            </li>
             <li class = "nav-item active">
                 <a class = "nav-link white-text" href = "maquinaria.php">Mantenimiento</a>
             </li>
             <li class = "nav-item active">
                 <a class = "nav-link white-text" href = "produccion.php">Proceso</a>
-            </li>         
+            </li>  
+            <li class = "nav-item active">
+                <a class = "nav-link white-text" href = "temperatura.php">Temperatura</a>
+            </li>
         </ul>
         <ul class = "navbar-nav nav-izq">
             <li class = "nav-item active">
-                <a class = "nav-link white-text" href = "#">Salir</a>
+                <a class = "nav-link white-text" href = "index.php">Salir</a>
             </li>  
         </ul>     
     </div>
