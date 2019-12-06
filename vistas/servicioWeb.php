e4e4b26125db406854e128c2b891b60b21048276
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-12-06 15:28:15 -0600

    Vista del servicio web

---
+++ vistas/servicioWeb.php
@@ -0,0 +1,9 @@
+<div class='form-centrado form-group' align="center">
+	<fieldset>
+		<legend align='center'>Información del servicio</legend><br>                
+        <form autocomplete='off' method='POST' action='ws/cliente.php'>
+			<input type='text' class='form-control-lg' id='ip' name='ip' placeholder='IP de servidor'><br><br>
+			<button type='submit' class='btn btn-outline-success'>Enviar</button>
+		</form>
+	</fieldset>
+</div>
