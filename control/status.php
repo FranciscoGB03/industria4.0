729762756cfd4ef8ff868fee4f84bb1a4f29571c
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-12-06 14:01:04 -0600

    Reestructuración de tabla HTML

--- control/status.php
+++ control/status.php
@@ -12,24 +12,41 @@ if ($consulta->num_rows > 0) {
     $tabla .= "<table class='table table-bordered table-hover'>
 					<thead>
 						<tr class='table-warning'>
+							<td align='center' colspan='2'>
+								<b>ESTADO ACTUAL</b>
+							</td>
+						</tr>
+						<tr class='table-warning'>
 							<td align='center'><b>Contador</b></td>
 							<td align='center'><b>Maquinaria</b></td>
-                                                        <td align='center'><b>Fecha</b></td>
 						</tr>
 					</thead>
 					<tbody>";
     while ($filamaq = $consulta->fetch_assoc()) {
         $tabla .= "<tr>
-						<td align='center'>" . $filamaq['status.cont'] . "</td>
-						<td align='center'>" . $filamaq['maquinaria.nombre'] . "</td>
-                                                <td align='center'>" . $filamaq['status.fecha'] . "</td>
+						<td align='center'>" . $filamaq['cont'] . "</td>
+						<td align='center'>" . $filamaq['nombre'] . "</td>
 					</tr>";
     }
 
     $tabla .= "	</tbody>
 				</table";
 } else {
-    $tabla = "SIN DATOS";
+    $tabla = "<table class='table table-bordered table-hover'>
+					<thead>
+						<tr class='table-warning'>
+							<td align='center' colspan='2'>
+								<b>ESTADO ACTUAL</b>
+							</td>
+						</tr>
+						<tr class='table-warning'>
+							<td align='center'><b>Contador</b></td>
+							<td align='center'><b>Maquinaria</b></td>
+						</tr>
+					</thead>
+					<tbody>
+					</tbody>
+					</table>";
 }
 
 echo $tabla;
