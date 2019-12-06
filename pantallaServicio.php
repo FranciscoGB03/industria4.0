a2ab44f18bd67f377300803f0d5de911f385f35e
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-12-06 15:27:28 -0600

    Interfaz del servicio web

---
+++ pantallaServicio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Industria 4.0</title>    
+        <?php include("componentes/enlaces.php"); ?>
+    </head>
+    <body>
+        <div>
+            <?php
+                include('vistas/navbar.php');
+            ?>
+        </div>
+        <div class="div-centrado">
+            <div>
+                <?php
+                    include('vistas/servicioWeb.php');
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
