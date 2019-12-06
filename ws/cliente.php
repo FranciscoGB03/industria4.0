c6a15f84b3bba4bce2ad81ea2734a9a93d121f0b
Montserrath Olivo <38671806+MontserrathOG@users.noreply.github.com>
2019-12-06 15:50:29 -0600

    web service cliente

--- ws/cliente.php
+++ ws/cliente.php
@@ -1,28 +1,13 @@
 <?php
-    require_once "../resources/librerias/NuSOAP/lib/nusoap.php";
-    $cliente = new nusoap_client("http://localhost/industria4/ws/webservice.php");
-      
-    $error = $cliente->getError();
-    if ($error) {
-        echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
-    }
-      
-    $result = $cliente->call("getProd", array("categoria" => "libros"));
-      
-    if ($cliente->fault) {
-        echo "<h2>Fault</h2><pre>";
-        print_r($result);
-        echo "</pre>";
-    }
-    else {
-        $error = $cliente->getError();
-        if ($error) {
-            echo "<h2>Error</h2><pre>" . $error . "</pre>";
-        }
-        else {
-            echo "<h2>Libros</h2><pre>";
-            echo $result;
-            echo "</pre>";
-        }
-    }
+   $client = new SoapClient(null, 
+    array('location'=>"http://192.168.207.11/servicios/server.php", 
+        'uri' => "http://192.168.207.11/servicios/server.php",
+        'trace'=>1 ));
+   try{
+    echo $return=$client->__soapCall("saludar",["Montse"]);
+   }catch(SoapFault $f){
+        //echo $f->getMessage().PHP_EOL;
+        print($client->__getLastResponse());
+
+   }
 ?>
