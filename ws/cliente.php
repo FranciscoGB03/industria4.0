bd98bd790c5b83bb826019782552e04747fdcd5e
FranciscoGB03 <fgonalezb@toluca.tecnm.mx>
2019-12-08 18:52:50 -0600

    webservices final

--- ws/cliente.php
+++ ws/cliente.php
@@ -5,12 +5,19 @@
    $condition='id=1';
    $consulta=$query->ConsultaUsuario($tabla, $condition);   
    $ip=$consulta['ip'];
+   //obtencion del lote
+   $consultalote=$query->ConsultaUltimoLote()->fetch_assoc();   
+   $numLote=$consultalote['numLote'];   
+   //obtencion de el numero de latas
+   $numlatas=$query->ConsultaLataUltimoLote()->fetch_assoc();
+   $latas=$numlatas['lata'];
+   //configuracion del servicio web
    $client = new SoapClient(null, 
     array('location'=>$ip, 
         'uri' => $ip,
         'trace'=>1 ));
    try{
-    $return=$client->__soapCall("datos",[2,15]);
+    $return=$client->__soapCall("datos",[$numLote,$latas]);
     if ($return==1){
         echo "<script>alert('envio de datos correctamente!')</script>";
         echo "<script>window.location='../pantallaServicio.php'</script>";        
